0bb7bee0703b3a97326d2eeb9d97dfef21900556
zorixid <elijah.dufour@outlook.com>
2024-10-13 23:34:56 -0400

    chore: fix the eslint error

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,11 +1,16 @@
 import { ContactUs } from "@/components/contact-us";
 import Slider from "@/components/slider";
+import { slidesData } from "@/data";
 
 export default function ContactUsPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between gap-10 mb-16">
       <div className="flex flex-col w-full h-[350px] sm:h-[578px]">
-        <Slider />
+        <Slider
+          slidesData={slidesData}
+          direction={"horizontal"}
+          navigation={true}
+        />
       </div>
       <div className="flex flex-col items-center justify-start text-left max-w-[1150px] px-7 sm:px-10 md:px-20 gap-16">
         <ContactUs />
